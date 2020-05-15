6eff9c3137a9bbd4e9a87d0f83d7b143f687acf6
PrasadKadu556 <64204226+PrasadKadu556@users.noreply.github.com>
2020-05-15 12:08:56 +0530

    Assignment
    Project

---
+++ Chache_Inverse.R
@@ -0,0 +1,26 @@
+#Inverse Code
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(i) inverse <- i
+  getinverse <- function() inverse
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+#function
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached inverse")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
+}
